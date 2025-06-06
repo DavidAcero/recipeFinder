1ab3fd04d9e55b6144972060b2a7a8674a5512ec
David Acero Trevethan <acerd@Davids-MacBook-Pro-2.local>
2025-06-06 08:25:19 -0600

    Playwright has the npm start option

--- playwright.config.ts
+++ playwright.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+  
     // {
     //   name: 'firefox',
     //   use: { ...devices['Desktop Firefox'] },
@@ -71,9 +71,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
